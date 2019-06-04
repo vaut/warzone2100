777279c4b51338cb155f8a5607b2d32d0ebe9a79
past-due <30942300+past-due@users.noreply.github.com>
2019-06-04 18:07:18 -0400

    Improve resources cleanup
    (Especially graphics-related resources.)

--- src/init.cpp
+++ src/init.cpp
@@ -755,6 +755,7 @@ void systemShutdown()
 {
 	pie_ShutdownRadar();
 	clearLoadedMods();
+	flushConsoleMessages();
 
 	shutdownEffectsSystem();
 	wzSceneEnd(nullptr);  // Might want to end the "Main menu loop" or "Main game loop".
@@ -790,6 +791,7 @@ void systemShutdown()
 	pal_ShutDown();		// currently unused stub
 	frameShutDown();	// close screen / SDL / resources / cursors / trig
 	screenShutDown();
+	gfx_api::context::get().shutdown();
 	cleanSearchPath();	// clean PHYSFS search paths
 	debug_exit();		// cleanup debug routines
 	PHYSFS_deinit();	// cleanup PHYSFS (If failure, state of PhysFS is undefined, and probably badly screwed up.)
@@ -1183,6 +1185,8 @@ bool stageTwoShutDown()
 	{
 		return false;
 	}
+
+	shutdown3DView();
 
 	return true;
 }
