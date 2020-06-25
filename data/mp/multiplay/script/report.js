ef3ddf101aedaa15224f2183328328e925e8232d
Vitya Andreev <andrvaut@ya.ru>
2020-06-25 14:02:38 +0300

    fix code style and update end report

--- data/mp/multiplay/script/report.js
+++ data/mp/multiplay/script/report.js
@@ -23,48 +23,48 @@ function printReportBattle()
 			console(_("the battle is over, you can leave the room"));
 			debug("the battle is over, you can leave the room");
 		}
-
 }
 
 namespace("rp_");
 function printStartGameSettings()
 {
-//add human readable method
-var human = {
-	scavengers : function () {
-		if ( scavengers == true) {return _("Scavengers");}
-		if ( scavengers == false) {return _("No Scavengers");}
+	//add human readable method
+	var human =
+	{
+		scavengers : function () {
+			if ( scavengers == true) {return _("Scavengers");}
+			if ( scavengers == false) {return _("No Scavengers");}
 		},
 
-	alliancesType : function () {
-		switch (alliancesType) {
-			case NO_ALLIANCES: return _("No Alliances");
-			case ALLIANCES: return _("Allow Alliances");
-			case ALLIANCES_TEAMS: return _("Locked Teams");
-			case ALLIANCES_UNSHARED: return _("Locked Teams, No Shared Research");
+		alliancesType : function () {
+			switch (alliancesType) {
+				case NO_ALLIANCES: return _("No Alliances");
+				case ALLIANCES: return _("Allow Alliances");
+				case ALLIANCES_TEAMS: return _("Locked Teams");
+				case ALLIANCES_UNSHARED: return _("Locked Teams, No Shared Research");
 			}
 		},
 
-	powerType : function () {
-		switch (powerType) {
-			case 0: return _("Low Power Levels");
-			case 1: return _("Medium Power Levels");
-			case 2: return _("High Power Levels");
+		powerType : function () {
+			switch (powerType) {
+				case 0: return _("Low Power Levels");
+				case 1: return _("Medium Power Levels");
+				case 2: return _("High Power Levels");
 			}
 		},
 
-	baseType : function () {
-		switch (baseType) {
-			case CAMP_CLEAN: return _("Start with No Bases");
-			case CAMP_BASE: return _("Start with Bases");
-			case CAMP_WALLS: return _("Start with Advanced Bases");
+		baseType : function () {
+			switch (baseType) {
+				case CAMP_CLEAN: return _("Start with No Bases");
+				case CAMP_BASE: return _("Start with Bases");
+				case CAMP_WALLS: return _("Start with Advanced Bases");
 			}
 		},
 	};
-
 	debug(JSON.stringify( [mapName, human.scavengers(), human.alliancesType(), human.powerType(), human.baseType(), "T" + getMultiTechLevel()]));
 	console( [mapName, human.scavengers(), human.alliancesType(), human.powerType(), human.baseType() ].join("\n"));
 }
+
 var attacker = [];
 function rp_eventGameInit()
 {
