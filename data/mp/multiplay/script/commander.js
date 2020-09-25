cd21705fe933d90d85f54b7630adbb5dae5e0552
Vitya Andreev <andrvaut@ya.ru>
2020-09-26 01:11:51 +0300

    save expearence

--- data/mp/multiplay/script/commander.js
+++ data/mp/multiplay/script/commander.js
@@ -1,7 +1,8 @@
 namespace("comm_");
-
+const BOOST = 4;
+const BOOST_RANGE = 4;
 var commandTurrets = [];
-var unitsBoost = [];
+var unitsOld = [];
 
 function comm_eventGameInit()
 {
@@ -10,32 +11,43 @@ function comm_eventGameInit()
 
 function updateBoost()
 {
-	unitsBoost.forEach(function (unit){
-	setDroidExperience(unit, -4);
-	debug("remove boost ",  unit.experience);
-	});
-	unitsBoost = [];
-
+	var units = [];
 	commandTurrets.forEach(function (turret){
 		turret = getObject(turret.type, turret.player, turret.id);
-		var objects = enumRange(turret.x, turret.y, 4);
-		var units = objects.filter(function (unit){
+		var objects = enumRange(turret.x, turret.y, BOOST_RANGE);
+		units = objects.filter(function (unit){
 			if (unit.type == DROID && unit.player == turret.player ){return true;}
 			return false;
 		});
-		debug (JSON.stringify(units));
-		units.forEach (function (unit){
-			unitsBoost.push(unit);
-			setDroidExperience(unit,  4);
-			debug("boost");
-		});
+		
+//		debug (JSON.stringify(units));
+//		units.forEach (function (unit){
+//			newBoost.push (unit);
+//			unit.boost+=1;
+//			setDroidExperience(unit, BOOST);
+//			debug("boost ", unit.experience);
+//		});
 	});
+
+//	debug ("new", JSON.stringify(units));
+//	debug ("old", JSON.stringify(unitsOld));
+	units.forEach(function (unit){
+		debug("check new");
+		if ( !contains(unitsOld, unit)){setDroidExperience(unit, Math.round(unit.experience+BOOST)); debug ("boost");}
+	});
+	unitsOld.forEach(function (unit){
+		debug("check remove");
+		if ( !contains(units, unit)){setDroidExperience(unit, Math.round(unit.experience-BOOST)); debug ("unboost");}
+
+	});
+	unitsOld = units;
+
 }
 
 function comm_eventDroidBuilt(droid)
 {	
 
-	debug (JSON.stringify(droid, null, '\t'));
+//	debug (JSON.stringify(droid, null, '\t'));
 	if (droid.droidType == DROID_COMMAND)
 	{
 		commandTurrets.push (droid);
@@ -43,4 +55,11 @@ function comm_eventDroidBuilt(droid)
 	}
 }
 
-
+function contains(arr, elem) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].id == elem.id) {
+            return true;
+        }
+    }
+    return false;
+}
