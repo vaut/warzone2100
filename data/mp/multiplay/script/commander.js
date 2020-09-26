4366b8e2b980a30bbea911ada1483b0274d7ddfd
Vitya Andreev <andrvaut@ya.ru>
2020-09-26 15:49:11 +0300

    update all object befo modifited

--- data/mp/multiplay/script/commander.js
+++ data/mp/multiplay/script/commander.js
@@ -6,7 +6,7 @@ var unitsOld = [];
 
 function comm_eventGameInit()
 {
-	setTimer("updateBoost", 500);
+	setTimer("updateBoost", 200);
 }
 
 function updateBoost()
@@ -14,30 +14,23 @@ function updateBoost()
 	var units = [];
 	commandTurrets.forEach(function (turret){
 		turret = getObject(turret.type, turret.player, turret.id);
+		if (turret === null){return;}
 		var objects = enumRange(turret.x, turret.y, BOOST_RANGE);
-		units = objects.filter(function (unit){
+		units = units.concat( objects.filter(function (unit){
 			if (unit.type == DROID && unit.player == turret.player ){return true;}
 			return false;
-		});
+		}));
 		
-//		debug (JSON.stringify(units));
-//		units.forEach (function (unit){
-//			newBoost.push (unit);
-//			unit.boost+=1;
-//			setDroidExperience(unit, BOOST);
-//			debug("boost ", unit.experience);
-//		});
 	});
-
-//	debug ("new", JSON.stringify(units));
-//	debug ("old", JSON.stringify(unitsOld));
 	units.forEach(function (unit){
-		debug("check new");
-		if ( !contains(unitsOld, unit)){setDroidExperience(unit, Math.round(unit.experience+BOOST)); debug ("boost");}
+		unit =  getObject(unit.type, unit.player, unit.id);
+		setDroidExperience(unit, Math.round(unit.experience+BOOST)); debug ("boost", unit.id);
 	});
 	unitsOld.forEach(function (unit){
-		debug("check remove");
-		if ( !contains(units, unit)){setDroidExperience(unit, Math.round(unit.experience-BOOST)); debug ("unboost");}
+		unit =  getObject(unit.type, unit.player, unit.id);
+		if (unit ===  null){return;}
+		if ( unit.experience < BOOST){debug( unit.id, "lost expearance");}
+		setDroidExperience(unit, Math.round(unit.experience-BOOST));  debug ("unboost", unit.id);
 
 	});
 	unitsOld = units;
@@ -53,13 +46,4 @@ function comm_eventDroidBuilt(droid)
 		commandTurrets.push (droid);
 		debug("push");
 	}
-}
-
-function contains(arr, elem) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].id == elem.id) {
-            return true;
-        }
-    }
-    return false;
 }
