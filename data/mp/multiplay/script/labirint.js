644ccfcff744018c8477c48db3742974e82f9a34
Vitya Andreev <andrvaut@ya.ru>
2020-12-31 14:38:02 +0300

    add randomise land and fix error

--- data/mp/multiplay/script/labirint.js
+++ data/mp/multiplay/script/labirint.js
@@ -21,26 +21,24 @@ function landing()
 	var avalibleTemplate = [];
 	for (var key in allTemplates)
 	{
-		if  (makeTemplate(AI, key, allTemplates[key].body, allTemplates[key].propulsion, "", allTemplates[key].weapons) !== null &&
+		if (!allTemplates[key].weapons){continue;}
+		if  (makeTemplate(AI, key, allTemplates[key].body, allTemplates[key].propulsion, "", allTemplates[key].weapons) !== null && //у makeTemplate изменен синтаксис в мастере. Не совместимо с 3.4.1
 		(allTemplates[key].propulsion != "wheeled01" || !componentAvailable( "HalfTrack")))
 		{
 			avalibleTemplate.push(key);
 		}
 	}
-//	debug (JSON.stringify(avalibleTemplate));
 
 	var budget = 1*4*gameTime/1000;
+	debug ("budget",budget);
 	while (budget >0)
 	{
 		var droidName = avalibleTemplate[syncRandom(avalibleTemplate.length)];
-		X = mapWidth/2;
-		Y = mapHeight/2;
-
-		var template = makeTemplate(AI, droidName, allTemplates[droidName].body, allTemplates[droidName].propulsion, "", allTemplates[droidName].weapons );
-//		debug(JSON.stringify(template));
+		X = mapWidth/2+syncRandom(1024)/128-4;
+		Y = mapHeight/2+syncRandom(1024)/128-4;
 		addDroid(AI, X, Y, droidName, allTemplates[droidName].body, allTemplates[droidName].propulsion , "", "", allTemplates[droidName].weapons );
-		budget -= template.power;
-		debug (budget);
+		budget -= makeTemplate(AI, droidName, allTemplates[droidName].body, allTemplates[droidName].propulsion , "", allTemplates[droidName].weapons).power;
+		debug("add", droidName);
 	}
 }
 
--- data/mp/multiplay/skirmish/Wave.js
+++ data/mp/multiplay/skirmish/Wave.js
@@ -1,7 +1,7 @@
-namespace("wawe_");
+namespace("wave_");
 debug ("run");
 
-function wawe_eventGameInit()
+function wave_eventGameInit()
 {
 	setTimer("attack", 10*1000);
 	debug ("init");
@@ -10,9 +10,11 @@ function wawe_eventGameInit()
 function attack()
 {
 	var targets = enumStruct(0).concat( enumDroid(0)) ;
-
+	if (targets.length == 0){return;}
 	var target = targets[syncRandom(targets.length)];
-	debug(JSON.stringify(target));
+	debug("target", target.name, target.x, target.y);
+	X = target.x+syncRandom(512)/128-2;
+	Y = target.y+syncRandom(512)/128-2;
 	var myWave = enumDroid(me, DROID_WEAPON);
-	myWave.forEach(function(o){orderDroidLoc(o, DORDER_SCOUT, target.x, target.y);});
+	myWave.forEach(function(o){orderDroidLoc(o, DORDER_SCOUT, X, Y);});
 }
